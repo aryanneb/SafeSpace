903dc9990f8d27ac477c09d35325a628ad81c812
aryanneb <aryanneb007@gmail.com>
2025-02-08 16:37:25 -0500

    tested build on mobile. worked!

--- SafeSpace/ContentView.swift
+++ SafeSpace/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, testing build on local phone!\n had to lower version requirements.")
         }
         .padding()
     }
