21c8013fcf623ddf6661cddd72cf8e65967781b6
aryanneb <aryanneb007@gmail.com>
2025-02-08 16:15:49 -0500

    Initial Commit

---
+++ SafeSpace/SafeSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSpaceApp.swift
+//  SafeSpace
+//
+//  Created by Aryan Neb on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct SafeSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SafeSpaceTests/SafeSpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSpaceTests.swift
+//  SafeSpaceTests
+//
+//  Created by Aryan Neb on 2025-02-08.
+//
+
+import Testing
+@testable import SafeSpace
+
+struct SafeSpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
